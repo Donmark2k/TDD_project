e49f25cc4c9a8d6bc8dc9c49ec2a74ddb4b25207
Donmark2k <chukwuemekavp@gmail.com>
2023-04-21 13:27:45 +0100

    Correct linters error

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
 class Solver
-    def factorial(number)
-        if number.negative?
-            'Please enter positive number or equal to 0!'
-        elsif [0, 1].include?(number)
-            1
-        else
-            number * factorial(number - 1)
-        end
+  def factorial(number)
+    if number.negative?
+      'Please enter positive number or equal to 0!'
+    elsif [0, 1].include?(number)
+      1
+    else
+      number * factorial(number - 1)
     end
-    
-    def fizzbuzz(n)
-        if n % 15 == 0
-          'fizzbuzz'
-        elsif n % 3 == 0
-          'fizz'
-        elsif n % 5 == 0
-          'buzz'
-        else
-          n.to_s
-        end
-      end
+  end
 
-    def reverse(word)
-        word.reverse
-      end
-      
+  def fizzbuzz(num)
+    if (num % 15).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
+
+  def reverse(word)
+    word.reverse
+  end
 end
