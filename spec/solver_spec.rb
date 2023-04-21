4f25de2d43046f64298faac403397c937d2b2d50
Donmark2k <chukwuemekavp@gmail.com>
2023-04-21 12:39:00 +0100

    Add test for reverse mehod

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,5 +20,46 @@ describe Solver do
     end
 
     # Reverse Method
+    describe '#reverse' do
+        it 'returns the reverse of a given string' do
+          expect(Solver.reverse('hello')).to eq('olleh')
+          expect(Solver.reverse('')).to eq('')
+          expect(Solver.reverse('12345')).to eq('54321')
+        end
+      end
+
+
+    # fizzbuzz Method
+
+      
+  describe '#fizzbuzz' do
+    context 'when given a number divisible by 3' do
+      it 'returns "fizz"' do
+        expect(Solver.fizzbuzz(3)).to eq('fizz')
+        expect(Solver.fizzbuzz(9)).to eq('fizz')
+      end
+    end
+    
+    context 'when given a number divisible by 5' do
+      it 'returns "buzz"' do
+        expect(Solver.fizzbuzz(5)).to eq('buzz')
+        expect(Solver.fizzbuzz(20)).to eq('buzz')
+      end
+    end
+    
+    context 'when given a number divisible by both 3 and 5' do
+      it 'returns "fizzbuzz"' do
+        expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+        expect(Solver.fizzbuzz(45)).to eq('fizzbuzz')
+      end
+    end
+    
+    context 'when given a number not divisible by 3 or 5' do
+      it 'returns the number as a string' do
+        expect(Solver.fizzbuzz(1)).to eq('1')
+        expect(Solver.fizzbuzz(7)).to eq('7')
+      end
+    end
+  end
 
 end
