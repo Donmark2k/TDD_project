ffe3023826389b42bf76fb7d5f2d6871fc331ac2
John Adibe <johnadibe123@gmail.com>
2023-04-21 12:58:34 -0800

    Add factorial method to solver.rb

--- solver.rb
+++ solver.rb
@@ -1,4 +1,14 @@
 class Solver
+    def factorial(number)
+        if number.negative?
+            'Please enter positive number or equal to 0!'
+        elsif [0, 1].include?(number)
+            1
+        else
+            number * factorial(number - 1)
+        end
+    end
+    
     def reverse(word)
         word.reverse
       end
